09ac472ce25ea3e7532b3a45501a326257cc6aa3
B0ND <mhashimkp@gmail.com>
2021-07-18 17:41:13 +0530

    Update and rename aosp_RMX1971.mk to wave_RMX1971.mk

--- wave_RMX1971.mk
+++ wave_RMX1971.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/wave/config/common.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,7 +15,7 @@ TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := aosp_RMX1971
+PRODUCT_NAME := wave_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
